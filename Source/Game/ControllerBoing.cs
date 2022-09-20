b09ced76ca591cfc6fb8856d77951af60d3c5f2e
Simonrazer <lpsimonrazer@gmail.com>
2022-09-20 09:17:13 +0200

    Init

---
+++ Source/Game/ControllerBoing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// ControllerBoing Script.
+    /// </summary>
+    public class ControllerBoing : Script
+    {
+        /// <inheritdoc/>
+        RigidBody rb;
+        public override void OnStart()
+        {
+            // Here you can add code that needs to be called when script is created, just before the first game update
+            rb = Actor.As<RigidBody>();
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+            // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+        }
+
+        /// <inheritdoc/>
+        public override void OnUpdate()
+        {
+            Vector3 wantedDir = new Vector3(Input.GetAxis("Horizontal")*500, Input.GetAxis("Vertical")*500, 0);
+            rb.LinearVelocity = wantedDir;
+        }
+    }
+}
